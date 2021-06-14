a50504872adb7de8f0fb224c3e61849fb488ed68
Barkley Santo <barkley.santo@gmail.com>
2021-06-14 15:44:38 -0500

    add seed data, average score method in Airline model, slugify method in Airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,2 +1,15 @@
 class Airline < ApplicationRecord
+  has_many :reviews
+
+  before_create :slugify
+
+  def slugify
+    # creates url safe version of the name for the slug
+    # hypehantes the spaces + lowercases everything
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    reviews.average(:score).round(2).to_f
+  end
 end
