5eacd368b2bb0d93a8180a89806e3f152aa383c8
Barkley Santo <barkley.santo@gmail.com>
2021-06-18 19:59:28 -0500

    create index and show methods for airlines controller, each method will render the return as json

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ root 'pages#index'
 
 namespace :api do
   namespace :v1 do 
-    resources :airlines, param: :slugresources :reviews, only: [:create, :destroy]
+    resources :airlines, param: :slug
+    resources :reviews, only: [:create, :destroy]
   end
 end
 
