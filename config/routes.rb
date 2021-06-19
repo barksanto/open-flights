4234421fc90886676b901900294402ecf5065a7c
Barkley Santo <barkley.santo@gmail.com>
2021-06-18 19:52:38 -0500

    setup routes, and routes for paths that dont have destination

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root 'pages#index'
+
+namespace :api do
+  namespace :v1 do 
+    resources :airlines, param: :slugresources :reviews, only: [:create, :destroy]
+  end
+end
+
+# routes requests that aren't for existing paths predefined in our api, 
+# back to our index path
+
+get '*path', to: 'pages#index', via: :all
+
 end
