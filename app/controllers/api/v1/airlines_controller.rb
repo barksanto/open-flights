d85e0916ebf9c89926d14500ecc23a554ea3f0c3
Barkley Santo <barkley.santo@gmail.com>
2021-06-18 20:02:03 -0500

    add save functionality to create - with an else that renders an error message

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -14,6 +14,25 @@ module Api
         render json: AirlineSerializer.new(airline).serialized_json
       end
 
+
+      def create
+        airline = Airline.new(airline_params)
+
+        if airline.save
+          render json: AirlineSerializer.new(airline).serialized_json
+        else
+          render json: {error: airline.errors.messages}, status:422
+        end
+
+      end
+
+      private
+
+      def airline_params
+        params.require(:airline).permit(:name, :image_irl)
+      end
+
+
     end
   end
 end
